12ac930cac3bdbf34f33192ca3a6789da8858d3a
J. Graves <jwgravesfl@gmail.com>
2019-01-15 15:53:17 -0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About Reveler Media Services</h1>
+      <p>Reveler Media Service</p>
+    </div>
+  </Layout>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout";
+
+const Index = props => (
+  <Layout>
+    <div>
+      <h1>
+        RMS Static Website Template
+        <br /> w/Server Side Rendering
+      </h1>
+      <p>Reveler Media Services Base SEO Template</p>
+    </div>
+  </Layout>
+);
+
+export default Index;
