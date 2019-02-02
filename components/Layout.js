9e19b628cc6888f1243ae5f8180644aa4dd3b37d
J. Graves <jwgravesfl@gmail.com>
2019-02-02 12:27:06 -0700

    added env and additional header information

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,12 @@ const Layout = props => (
   <div>
     <Head>
       <title>Reveler Media Services SEO Template</title>
+      <meta property="og:title" content="Reveler Media Services SEO Template" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="Generate a Canonical Pague URL" />
+      <meta property="og:image" content=" Image associated with the Page " />
+
+      {/* Bootstrap CDN, remove and add UI Library of choice */}
       <link
         rel="stylesheet"
         href="https://bootswatch.com/4/cerulean/bootstrap.min.css"
